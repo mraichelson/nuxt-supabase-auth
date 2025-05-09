1f1100a33864862e2bee2a403c55a2185bc1185d
Michael Raichelson <i.am@michaelraichelson.com>
2025-05-09 10:56:26 -0500

    not working yet.

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+<template>
+	<article>
+		<h1>Homepage</h1>
+		<p>
+			<NuxtLink to="/login">Login</NuxtLink>
+		</p>
+	</article>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const supabase = useSupabaseClient()
+
+const login = async () => {
+	console.log('LOGIN')
+
+	const { error } = await supabase.auth.signInWithOAuth({
+		provider: 'github',
+	})
+
+	if (error) {
+		console.error(error)
+	}
+}
+</script>
+<template>
+	<article>
+		<h1>Login screen</h1>
+
+		<button @click.prevent="login">Login with Github</button>
+	</article>
+</template>
