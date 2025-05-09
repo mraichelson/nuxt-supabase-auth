af310d6e59199f4546f489ab5444cabbe28325f2
Michael Raichelson <i.am@michaelraichelson.com>
2025-05-09 12:15:05 -0500

    add a layout file for no real reason

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="layout-wrapper">
+		<slot />
+	</div>
+</template>
+
+<style>
+.layout-wrapper {
+	font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+		Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+</style>
